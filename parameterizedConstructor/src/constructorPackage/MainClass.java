cf62536ee1d468f2e68ba4fa01645721a2c36524
Shanghaai <34318533+Shanghaai@users.noreply.github.com>
2017-12-16 01:17:37 +0530

    Add files via upload

---
+++ parameterizedConstructor/src/constructorPackage/MainClass.java
@@ -0,0 +1,26 @@
+package constructorPackage;
+
+class mainclass
+{
+public static void main(String[] args)
+{
+	   System.out.println("Game Started");
+	   System.out.println("");
+	   
+	   Sample1 s1 = new Sample1(21,22);
+	   
+	   System.out.println("x value"+s1.x);
+	   System.out.println("y value"+s1.y);
+	   System.out.println("Game Ended");
+	   System.out.println("");
+	   
+	   System.out.println("Game Started");
+	   
+	   Sample1 s2 = new Sample1(21,22);
+	   
+	   System.out.println("x value"+s2.x);
+	   System.out.println("y value"+s2.y);
+	   System.out.println("Game Ended");
+		   
+		   
+}}
---
+++ parameterizedConstructor/src/constructorPackage/Sample1.java
@@ -0,0 +1,15 @@
+package constructorPackage;
+
+public class Sample1
+	{
+	   int x;
+	   int y;
+	   
+	   Sample1(int x1,int x2)
+	   {
+		   System.out.println("making a constructor by using classname");
+		   x = x1;
+		   y = x2;
+	   }
+	}
+
